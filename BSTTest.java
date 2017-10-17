4f41703216a188e30c05c950dfaee98febe14043
dylanterry <dylanterry2@gmail.com>
2017-10-17 11:32:57 +0100

    more tests added

--- BSTTest.java
+++ BSTTest.java
@@ -15,7 +15,7 @@ public class BSTTest {
 		bst.put(6, 6);   //  \     /
 		bst.put(4, 4);   //   2   4
 		bst.put(5, 5);   //        \
-		bst.put(9, 9);				//         5
+		bst.put(9, 9);	//         5
 		
 		
 		return bst;
@@ -32,21 +32,21 @@ public class BSTTest {
 
 	}
 	@Test
-	public void testNullTree(){
-		BST<Integer, Integer> bst = new BST<Integer, Integer>();
+	public void testNullNode(){
+		BST<Integer, Integer> bst = newBST();
 		
-		bst.put(7, 7);   	//        _7_
-		bst.put(8, 8);   	//      /     \
-		bst.put(3, 3);   	//    _3_      8
-		bst.put(1, 1);   	//  /     \	 	\
-		bst.put(2, 2);   	// 1       6	  9
-		bst.put(6, 6);  	//  \     /
-		bst.put(4, 4);  	//   2   4
-		bst.put(5, 5);   	//        \
-		bst.put(9, 9);		//         5
+		assertEquals(null, bst.LCA(bst.get(7), bst.get(3), bst.get(22))); //testing out of bounds node
+	}
+	
+	@Test 
+	public void testNullRoot(){
+		BST <Integer, Integer> bst = new BST <Integer, Integer>();
 		
+		bst.put(null, null);
+		bst.put(1, 1);
+		bst.put(3, 2);
 		
-		assertEquals(null, bst.LCA(bst.get(7), bst.get(3), bst.get(22)));
+		assertEquals(null, bst.LCA(bst.get(2), bst.get(1),bst.get(3) ));
 	}
 
 }
