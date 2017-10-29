e3f675228e85018a1b62c8406d240593eca13da1
dylanterry <dylanterry2@gmail.com>
2017-10-29 14:22:38 +0000

    added a put function

--- DAG.java
+++ DAG.java
@@ -19,10 +19,6 @@ public class DAG <Value> {
 		else return false;
 	}
 	
-	public int size(){
-		return listNodes.length;
-	}
-	
 	public boolean contains (Value v ){
 		boolean contains = false;
 		int length = size();
@@ -35,5 +31,58 @@ public class DAG <Value> {
 		}
 		return contains;
 	}
+	
+	public int size(){
+		return listNodes.length;
+	}
+	
+	public void put (Value v, Value prevVal, Value nextVal){
+		Node node = new Node (v);
+		Node prev = getNodeFromVal(prevVal);
+		Node next = getNodeFromVal (nextVal);
+		
+		if (nextVal != null){
+			node.sucessors = arrayBigger(node.sucessors);
+			node.sucessors[node.sucessors.length-1] = next;
+			//if prev is not in graph
+			if (next.value == null){
+				addToListNodes(next);
+			}
+		}
+		
+		if (prevVal != null){
+			prev.sucessors = arrayBigger(prev.sucessors);
+			prev.sucessors[prev.sucessors.length-1] = node;
+			//if to node is not in graph
+			if (prev.value == null){
+				addToListNodes(prev);
+			}
+		}
+		
+		addToListNodes(node);
+	}
+	
+	public Node[] arrayBigger (Node[] Array) {
+		Node[] copyArray = new DAG.Node[Array.length+1];
+		System.arraycopy(Array, 0, copyArray, 0, Array.length);
+		return copyArray;
+	}
+	public void addToListNodes (Node n) {
+		listNodes = arrayBigger(listNodes);
+		listNodes[listNodes.length-1] = n;
+	}
+	
+	public Node getNodeFromVal (Value v ){
+		Node returnNode = new Node (null);
+		for (int i=0; i<size();i++){
+			if (listNodes[i].value == v){
+				returnNode = listNodes[i];
+				break;
+			}
+		}
+		return returnNode;
+	}
+	
+	
 
 }
