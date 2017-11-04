9b8ad6997726c7474f57c5e047da7ab6dff449e2
dylanterry <dylanterry2@gmail.com>
2017-11-04 10:39:25 +0000

    chnaging implementation of dag

--- DAG.java
+++ DAG.java
@@ -1,4 +1,5 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class DAG <Value> {
 	
@@ -11,76 +12,19 @@ public class DAG <Value> {
 			this.value = value;
 		}
 	}
+	private Node root;
 	
-	private Node [] listNodes = new DAG.Node[0];
-	
-	public boolean isEmpty (){
-		if (size()==0) return true;
-		else return false;
-	}
-	
-	public boolean contains (Value v ){
-		boolean contains = false;
-		int length = size();
-		for(int i=0;i<length;i++){
-			
-			if (listNodes[i].value == v){
-				contains = true;
-				break;
+	private ArrayList <Node> DFS (Node node, Node target, ArrayList <Node> list, Stack <Node> stack){
+		stack.push(node);
+		for (Node theNode:node.nodeList){
+			if (theNode.equals(target)){
+				list.addAll(stack);
+				return list;
 			}
+			DFS(theNode, target, list, stack);
 		}
-		return contains;
-	}
-	
-	public int size(){
-		return listNodes.length;
-	}
-	
-	public void put (Value v, Value prevVal, Value nextVal){
-		Node node = new Node (v);
-		Node prev = getNodeFromVal(prevVal);
-		Node next = getNodeFromVal (nextVal);
-		
-		if (nextVal != null){
-			node.sucessors = arrayBigger(node.sucessors);
-			node.sucessors[node.sucessors.length-1] = next;
-			//if prev is not in graph
-			if (next.value == null){
-				addToListNodes(next);
-			}
-		}
-		
-		if (prevVal != null){
-			prev.sucessors = arrayBigger(prev.sucessors);
-			prev.sucessors[prev.sucessors.length-1] = node;
-			//if to node is not in graph
-			if (prev.value == null){
-				addToListNodes(prev);
-			}
-		}
-		
-		addToListNodes(node);
-	}
-	
-	public Node[] arrayBigger (Node[] Array) {
-		Node[] copyArray = new DAG.Node[Array.length+1];
-		System.arraycopy(Array, 0, copyArray, 0, Array.length);
-		return copyArray;
-	}
-	public void addToListNodes (Node n) {
-		listNodes = arrayBigger(listNodes);
-		listNodes[listNodes.length-1] = n;
-	}
-	
-	public Node getNodeFromVal (Value v ){
-		Node returnNode = new Node (null);
-		for (int i=0; i<size();i++){
-			if (listNodes[i].value == v){
-				returnNode = listNodes[i];
-				break;
-			}
-		}
-		return returnNode;
+		stack.pop();
+		return list;
 	}
 	
 	
---
+++ DAGTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DAGTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	
+
+}
